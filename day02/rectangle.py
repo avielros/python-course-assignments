0e9463348dac57021a04080263d6bdfaefdef78b
avielros <aviro205@gmail.com>
2025-04-03 21:34:19 +0300

    Create rectangle.py

---
+++ day02/rectangle.py
@@ -0,0 +1,8 @@
+height= float(input("what is the rectangle's height"))
+width = float(input("what is the rectangle's width "))
+
+area= width  * height
+circumference= (2 *height) + (2* width)
+
+print(f"The rectangle's area is: {area}")
+print(f"The rectangle's area is: {circumference}")
