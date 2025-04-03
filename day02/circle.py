3dc204b1221e27d72b88466a5fb0e870103c1633
avielros <aviro205@gmail.com>
2025-04-03 21:30:20 +0300

    Update circle.py

--- day02/circle.py
+++ day02/circle.py
@@ -1,9 +1,9 @@
-radius= float(input("what is the circle's radius")
+radius= float(input("what is the circle's radius"))
 
 import math
 
 area= math.pi * radius **2
 circumference= 2 *math.pi * radius
 
-print(f"The circle's area is:" {area})
-print(f"The circumference's area is:" {circumference})
+print(f"The circle's area is: {area}")
+print(f"The circumference's area is: {circumference}")
