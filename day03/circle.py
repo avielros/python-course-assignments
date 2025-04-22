bfabf1d11f19fd52671f083d342ab7799003a119
avielros <aviro205@gmail.com>
2025-04-22 11:11:20 +0300

    Create circle.py

---
+++ day03/circle.py
@@ -0,0 +1,12 @@
+import sys
+
+radius= sys.argv[1]
+radius=int(radius)
+
+import math
+
+area= math.pi * radius **2
+circumference= 2 *math.pi * radius
+
+print(f"The circle's area is: {area}")
+print(f"The circumference's area is: {circumference}")
