33dd458251ad650a5697ac6ed7cd7a7cde079a1d
avielros <aviro205@gmail.com>
2025-04-22 11:13:55 +0300

    Create rectangle.py

---
+++ day03/rectangle.py
@@ -0,0 +1,10 @@
+import sys
+
+height= int(sys.argv[1])
+width = int(sys.argv[2])
+
+area= width  * height
+circumference= (2 *height) + (2* width)
+
+print(f"The rectangle's area is: {area}")
+print(f"The rectangle's area is: {circumference}")
