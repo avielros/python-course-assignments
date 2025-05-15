111cafa9f19b4582483db678ca2a52014c0bf5e4
avielros <aviro205@gmail.com>
2025-05-15 19:30:45 +0300

    Create number_guessing_game_2.py

---
+++ day04/number_guessing_game_2.py
@@ -0,0 +1,21 @@
+import random
+
+sec_num = random.randrange(1, 21)
+guess = None
+
+while guess != sec_num:
+    guess = input("Enter your guess: a number between 1 to 20 (or 'x' to quit): ")
+
+    if guess == 'x':
+        print("You chose to exit")
+        break
+
+    print("You chose: " + guess)
+    guess = int(guess)
+
+    if guess == sec_num:
+        print("You are right!")
+    elif guess > sec_num:
+        print("Too high!")
+    else:
+        print("Too low!")
