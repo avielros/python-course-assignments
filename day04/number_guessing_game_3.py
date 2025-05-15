b06802e50ee032742a0f0fa2c7b09b3a1756bc08
avielros <aviro205@gmail.com>
2025-05-15 19:34:07 +0300

    Create number_guessing_game_3.py

---
+++ day04/number_guessing_game_3.py
@@ -0,0 +1,25 @@
+import random
+
+sec_num = random.randrange(1, 21)
+guess = None
+
+while guess != sec_num:
+    guess = input("Enter your guess: a number between 1 to 20 (or 'x' to quit, 's' to show the secret number): ")
+
+    if guess == 'x':
+        print("You chose to exit. Goodbye!")
+        break
+
+    if guess == 's':
+        print(f"(you cheated) The secret number is: {sec_num}")
+        continue  
+
+    print("You chose: " + guess)
+    guess = int(guess)
+
+    if guess == sec_num:
+        print("You are right!")
+    elif guess > sec_num:
+        print("Too high!")
+    else:
+        print("Too low!")
